ae75c04ed52debd0aafe16627c17965e46dc5579
Lalitpandey1 <lalitpandey4all@gmail.com>
2025-04-09 02:48:11 +0530

    Initial commit - TAGGER setup with Vite, React, Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600 p-8">
+      Hello, TAGGER is Alive! 🚀
+    </div>
+  );
+}
+
+export default App;
